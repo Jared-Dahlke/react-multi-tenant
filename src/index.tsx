843bf87cc8229e38543d0a22649eb5ba9a1603d8
Jared Dahlke <jared@sightly.com>
2020-08-31 14:54:30 -0700

    remove warning by implementing materialUI theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Material UI v4 has a known warning bug when using some menus components, such as the menu items on the AdminNavBarLinks component. Without using this ThemeProvider wrapper, AdminNavBarLinks component
+// will issue a "Warning: findDOMNode is deprecated in StrictMode" warning when those menus are used. Per https://github.com/mui-org/material-ui/issues/13394, Material UI
+// plans to fix this bug in V5, at which point we can upgrade to v5 then remove this wrapper. -Jared
+import { unstable_createMuiStrictModeTheme, ThemeProvider } from '@material-ui/core/styles';
+const theme = unstable_createMuiStrictModeTheme();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
