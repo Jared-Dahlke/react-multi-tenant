500df67def8c9b3aa3b5f41a3d5e32aba3f603f7
Jared Dahlke <jared@sightly.com>
2020-09-20 14:49:19 -0700

    styled custom alert

--- src/components/CustomAlert.js
+++ src/components/CustomAlert.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+//import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { makeStyles } from "@material-ui/core/styles";
+import Button from '../components/CustomButtons/Button'
+
+import {defaultFont, whiteColor} from '../assets/jss/material-dashboard-react.js'
+
+const useStyles = makeStyles((theme) => ({
+  paperStyle: {
+    backgroundColor: whiteColor,
+    ...defaultFont
+  }
+}));
 
 export default function AlertDialog(props) {
+
+  const classes = useStyles();
 
   return (
   
@@ -15,6 +28,9 @@ export default function AlertDialog(props) {
       onClose={props.handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
+      PaperProps={{
+        className: classes.paperStyle
+      }}
     >
       <DialogTitle id="alert-dialog-title">{props.titleText}</DialogTitle>
       <DialogContent>
@@ -23,7 +39,7 @@ export default function AlertDialog(props) {
         </DialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button onClick={props.handleClose} color="primary">
+        <Button onClick={props.handleClose}>
           {props.cancelText}
         </Button>
         <Button onClick={props.handleConfirm} color="primary" autoFocus>
