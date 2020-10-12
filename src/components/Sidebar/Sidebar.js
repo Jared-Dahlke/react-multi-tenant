d8c8d35d01b0d3a49c84f750235e5741afc1bc91
Jared Dahlke <jared@sightly.com>
2020-10-12 13:42:28 -0700

    organize discover sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,266 +1,340 @@
 /*eslint-disable*/
-import React from "react";
-import classNames from "classnames";
-import PropTypes from "prop-types";
-import { NavLink } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import Hidden from "@material-ui/core/Hidden";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import Collapse from '@material-ui/core/Collapse';
-import AdminNavbarLinks from "../../components/Navbars/AdminNavbarLinks.js";
-import styles from "../../assets/jss/material-dashboard-react/components/sidebarStyle.js";
+import React from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import Drawer from '@material-ui/core/Drawer'
+import Hidden from '@material-ui/core/Hidden'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import Collapse from '@material-ui/core/Collapse'
+import AdminNavbarLinks from '../../components/Navbars/AdminNavbarLinks.js'
+import styles from '../../assets/jss/material-dashboard-react/components/sidebarStyle.js'
 import Settings from '@material-ui/icons/Settings'
 import PieChart from '@material-ui/icons/PieChart'
-import SettingsRoutes from '../../routes'
+import { SettingsRoutes, DiscoverRoutes } from '../../routes'
 import AccountDropdown from '../../components/AccountDropdown'
+import WbIncandescentIcon from '@material-ui/icons/WbIncandescent'
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(styles)
 
 export default function Sidebar(props) {
-  const classes = useStyles();
-  // verifies if routeName is the one active (in browser input)
-  function activeRoute(routeName) {
-    let current = window.location.href
-    return current.includes(routeName)
-  }
+	const classes = useStyles()
+	// verifies if routeName is the one active (in browser input)
+	function activeRoute(routeName) {
+		let current = window.location.href
+		return current.includes(routeName)
+	}
 
-  const [openCollapse, setOpenCollapse] = React.useState(true);    
+	const [openCollapse, setOpenCollapse] = React.useState(true)
+	const [openCollapseDiscover, setOpenCollapseDiscover] = React.useState(true)
 
-  function handleOpenSettings(){
-     setOpenCollapse(!openCollapse);
-  }
+	function handleOpenSettings() {
+		setOpenCollapse(!openCollapse)
+	}
 
+	function handleOpenDiscover() {
+		setOpenCollapseDiscover(!openCollapseDiscover)
+	}
 
-  const { color, logo, image, logoText, routes } = props;
+	const { color, logo, image, logoText, routes } = props
 
-  
+	const listItemClasses = classNames({
+		[' ' + classes[color]]: activeRoute('/admin/settings')
+	})
 
-  const listItemClasses = classNames({
-    [" " + classes[color]]: activeRoute('/admin/settings')
-  });
+	const listItemClassesDiscover = classNames({
+		[' ' + classes[color]]: activeRoute('/admin/discover')
+	})
 
-  const listItemClassesDiscovery = classNames({
-    [" " + classes[color]]: activeRoute('/admin/discover')
-  });
+	const whiteFontClasses = classNames({
+		[' ' + classes.whiteFont]: true
+	})
 
-  const whiteFontClasses = classNames({
-    [" " + classes.whiteFont]: true
-  });
+	var links = (
+		<div>
+			{/**begin discover slinky */}
 
-  var links = (
-    <div>
+			<List className={classes.list}>
+				<ListItem
+					onClick={handleOpenDiscover}
+					button
+					className={classes.itemLink + listItemClassesDiscover}
+				>
+					<PieChart
+						className={classNames(classes.itemIcon, whiteFontClasses)}
+					/>
 
-    <List className={classes.list}>
-      <NavLink
-        to={'/admin/discover/home'}
-        className={classes.item}
-        activeClassName="active"
-        key={'1'}          
-      >     
-        <div className="discoverLink">
-          <ListItem  button className={classes.itemLink + listItemClassesDiscovery}>         
-          
-            <PieChart
-              className={classNames(classes.itemIcon, whiteFontClasses)}
-            />
-            <ListItemText
-              primary={'Discover'}
-              className={classNames(classes.itemText, whiteFontClasses)}
-              disableTypography={true}
-            />          
-          </ListItem>
-        </div>
-      </NavLink>
-    </List>
+					<ListItemText
+						primary={'Discover'}
+						className={classNames(classes.itemText, whiteFontClasses)}
+						disableTypography={true}
+					/>
+					{openCollapseDiscover ? (
+						<ExpandLess
+							className={classNames(classes.itemIcon, whiteFontClasses)}
+						/>
+					) : (
+						<ExpandMore
+							className={classNames(classes.itemIcon, whiteFontClasses)}
+						/>
+					)}
+				</ListItem>
 
-    <List className={classes.list}>
-    
+				<Collapse in={openCollapseDiscover} timeout='auto' unmountOnExit>
+					<List>
+						{DiscoverRoutes.map((setting, key) => {
+							const whiteFontClassesNest = classNames({
+								[' ' + classes.whiteFont]: true
+							})
 
-            <ListItem onClick={handleOpenSettings} button className={classes.itemLink + listItemClasses}>
-              
-             
-                <Settings
-                  className={classNames(classes.itemIcon, whiteFontClasses)}
-                />
-           
-              <ListItemText
-                primary={'Settings'}
-                className={classNames(classes.itemText, whiteFontClasses)}
-                disableTypography={true}
-              />
-              {openCollapse ? <ExpandLess className={classNames(classes.itemIcon, whiteFontClasses)}/> : <ExpandMore className={classNames(classes.itemIcon, whiteFontClasses)}/>}
-            </ListItem>
+							const subListItemClasses = classNames({
+								[' ' + classes[color]]: activeRoute(
+									setting.layout + setting.path
+								)
+							})
 
-            <Collapse in={openCollapse} timeout="auto" unmountOnExit>
-              <List>
+							let itemClass = classes.nestedItemLink
+							//if(setting.path === '/settings/users' || setting.path === '/settings/rolesPermissions' || setting.path === '/settings/brandProfiles' || setting.path === '/settings/brandMentality') {
+							//  itemClass = classes.subNestedItemLink
+							//}
 
-                {SettingsRoutes.map((setting, key)=>{
+							return (
+								<NavLink
+									to={setting.layout + setting.path}
+									className={classes.item}
+									activeClassName='active'
+									key={key}
+								>
+									<div
+										className={
+											setting.path.includes('brand')
+												? 'brandProfileLink'
+												: 'test'
+										}
+									>
+										<ListItem
+											button
+											inset='true'
+											className={itemClass + subListItemClasses}
+										>
+											<setting.icon
+												style={{
+													color: 'white',
+													width: '24px',
+													height: '30px',
+													fontSize: '24px',
+													lineHeight: '30px',
+													float: 'left',
+													marginRight: '15px',
+													marginLeft: '15px',
+													textAlign: 'center',
+													verticalAlign: 'middle'
+												}}
+											/>
 
-                  const whiteFontClassesNest = classNames({
-                    [" " + classes.whiteFont]: true
-                  });
+											<ListItemText
+												primary={setting.name}
+												className={classNames(
+													classes.itemText,
+													whiteFontClassesNest
+												)}
+												disableTypography={true}
+											/>
+										</ListItem>
+									</div>
+								</NavLink>
+							)
+						})}
+					</List>
+				</Collapse>
+			</List>
 
-                  const subListItemClasses = classNames({
-                    [" " + classes[color]]: activeRoute(setting.layout + setting.path)
-                  });
+			{/**end discover slinky , begin settings slinky*/}
 
-                  let itemClass = classes.nestedItemLink
-                  //if(setting.path === '/settings/users' || setting.path === '/settings/rolesPermissions' || setting.path === '/settings/brandProfiles' || setting.path === '/settings/brandMentality') {
-                  //  itemClass = classes.subNestedItemLink
-                  //}
+			<List className={classes.list}>
+				<ListItem
+					onClick={handleOpenSettings}
+					button
+					className={classes.itemLink + listItemClasses}
+				>
+					<Settings
+						className={classNames(classes.itemIcon, whiteFontClasses)}
+					/>
 
-                  return (
+					<ListItemText
+						primary={'Settings'}
+						className={classNames(classes.itemText, whiteFontClasses)}
+						disableTypography={true}
+					/>
+					{openCollapse ? (
+						<ExpandLess
+							className={classNames(classes.itemIcon, whiteFontClasses)}
+						/>
+					) : (
+						<ExpandMore
+							className={classNames(classes.itemIcon, whiteFontClasses)}
+						/>
+					)}
+				</ListItem>
 
-                    <NavLink
-                      to={setting.layout + setting.path}
-                      className={classes.item}
-                      activeClassName="active"
-                      key={key}   
-                       
-                    >
-                      <div className={setting.path.includes('brand') ? 'brandProfileLink':'test'}>
+				<Collapse in={openCollapse} timeout='auto' unmountOnExit>
+					<List>
+						{SettingsRoutes.map((setting, key) => {
+							const whiteFontClassesNest = classNames({
+								[' ' + classes.whiteFont]: true
+							})
 
-                      <ListItem button inset="true" className={itemClass + subListItemClasses}>
-                        
-                     
+							const subListItemClasses = classNames({
+								[' ' + classes[color]]: activeRoute(
+									setting.layout + setting.path
+								)
+							})
 
-                          <setting.icon 
-                            style={{color: 'white',width: "24px",
-                            height: "30px",
-                            fontSize: "24px",
-                            lineHeight: "30px",
-                            float: "left",
-                            marginRight: "15px",
-                            marginLeft:'15px',
-                            textAlign: "center",
-                            verticalAlign: "middle",
-                            }}
-                          />
+							let itemClass = classes.nestedItemLink
+							//if(setting.path === '/settings/users' || setting.path === '/settings/rolesPermissions' || setting.path === '/settings/brandProfiles' || setting.path === '/settings/brandMentality') {
+							//  itemClass = classes.subNestedItemLink
+							//}
 
-                          
-                
-                        <ListItemText  
-                          primary={setting.name} 
-                          className={classNames(classes.itemText, whiteFontClassesNest)}
-                          disableTypography={true}
-                        />
-                      </ListItem>
+							return (
+								<NavLink
+									to={setting.layout + setting.path}
+									className={classes.item}
+									activeClassName='active'
+									key={key}
+								>
+									<div
+										className={
+											setting.path.includes('brand')
+												? 'brandProfileLink'
+												: 'test'
+										}
+									>
+										<ListItem
+											button
+											inset='true'
+											className={itemClass + subListItemClasses}
+										>
+											<setting.icon
+												style={{
+													color: 'white',
+													width: '24px',
+													height: '30px',
+													fontSize: '24px',
+													lineHeight: '30px',
+													float: 'left',
+													marginRight: '15px',
+													marginLeft: '15px',
+													textAlign: 'center',
+													verticalAlign: 'middle'
+												}}
+											/>
 
-                      </div>
+											<ListItemText
+												primary={setting.name}
+												className={classNames(
+													classes.itemText,
+													whiteFontClassesNest
+												)}
+												disableTypography={true}
+											/>
+										</ListItem>
+									</div>
+								</NavLink>
+							)
+						})}
+					</List>
+				</Collapse>
+			</List>
+		</div>
+	)
 
-                    </NavLink>
+	var brand = (
+		<div className={classes.logo}>
+			<a
+				href='https://www.sightly.com/'
+				className={classNames(classes.logoLink, {
+					[classes.logoLinkRTL]: props.rtlActive
+				})}
+				target='_blank'
+			>
+				<div className={classes.logoImage}>
+					<img src={logo} alt='logo' className={classes.img} />
+				</div>
+				{logoText}
+			</a>
 
-                  )
+			<AccountDropdown />
+		</div>
+	)
+	return (
+		<div>
+			<Hidden mdUp implementation='css'>
+				<Drawer
+					variant='temporary'
+					anchor={props.rtlActive ? 'left' : 'right'}
+					open={props.open}
+					classes={{
+						paper: classNames(classes.drawerPaper, {
+							[classes.drawerPaperRTL]: props.rtlActive
+						})
+					}}
+					onClose={props.handleDrawerToggle}
+					ModalProps={{
+						keepMounted: true // Better open performance on mobile.
+					}}
+				>
+					{brand}
 
-                })}
+					<div className={classes.sidebarWrapper}>
+						<AdminNavbarLinks />
+						{links}
+					</div>
+					{image !== undefined ? (
+						<div
+							className={classes.background}
+							style={{ backgroundImage: 'url(' + image + ')' }}
+						/>
+					) : null}
+				</Drawer>
+			</Hidden>
+			<Hidden smDown implementation='css'>
+				<Drawer
+					anchor={props.rtlActive ? 'right' : 'left'}
+					variant='permanent'
+					open
+					classes={{
+						paper: classNames(classes.drawerPaper, {
+							[classes.drawerPaperRTL]: props.rtlActive
+						})
+					}}
+				>
+					{brand}
 
-                
-              </List>
-            </Collapse>
-
-    
-     
-    </List>
-
-
-
-
-</div>
-  );
-
-  var brand = (
-    <div className={classes.logo}>
-      <a
-        href="https://www.sightly.com/"
-        className={classNames(classes.logoLink, {
-          [classes.logoLinkRTL]: props.rtlActive
-        })}
-        target="_blank"
-      >
-        <div className={classes.logoImage}>
-          <img src={logo} alt="logo" className={classes.img} />
-        </div>
-        {logoText}
-      </a>
-
-      
-        <AccountDropdown/>
-      
-
-    </div>
-  );
-  return (
-    <div>
-      <Hidden mdUp implementation="css">
-        <Drawer
-          variant="temporary"
-          anchor={props.rtlActive ? "left" : "right"}
-          open={props.open}
-          classes={{
-            paper: classNames(classes.drawerPaper, {
-              [classes.drawerPaperRTL]: props.rtlActive
-            })
-          }}
-          onClose={props.handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true // Better open performance on mobile.
-          }}
-        >
-          {brand}
-         
-         
-
-          <div className={classes.sidebarWrapper}>
-             <AdminNavbarLinks />
-            {links}
-          </div>
-          {image !== undefined ? (
-            <div
-              className={classes.background}
-              style={{ backgroundImage: "url(" + image + ")" }}
-            />
-          ) : null}
-        </Drawer>
-      </Hidden>
-      <Hidden smDown implementation="css">
-        <Drawer
-          anchor={props.rtlActive ? "right" : "left"}
-          variant="permanent"
-          open
-          classes={{
-            paper: classNames(classes.drawerPaper, {
-              [classes.drawerPaperRTL]: props.rtlActive
-            })
-          }}
-        >
-          {brand}
-          
-          <div className={classes.sidebarWrapper}>{links}</div>
-          {image !== undefined ? (
-            <div
-              className={classes.background}
-              style={{ backgroundImage: "url(" + image + ")" }}
-            />
-          ) : null}
-        </Drawer>
-      </Hidden>
-    </div>
-  );
+					<div className={classes.sidebarWrapper}>{links}</div>
+					{image !== undefined ? (
+						<div
+							className={classes.background}
+							style={{ backgroundImage: 'url(' + image + ')' }}
+						/>
+					) : null}
+				</Drawer>
+			</Hidden>
+		</div>
+	)
 }
 
 Sidebar.propTypes = {
-  rtlActive: PropTypes.bool,
-  handleDrawerToggle: PropTypes.func,
-  bgColor: PropTypes.oneOf(["purple", "blue", "green", "orange", "red"]),
-  logo: PropTypes.string,
-  image: PropTypes.string,
-  logoText: PropTypes.string,
-  routes: PropTypes.arrayOf(PropTypes.object),
-  open: PropTypes.bool
-};
+	rtlActive: PropTypes.bool,
+	handleDrawerToggle: PropTypes.func,
+	bgColor: PropTypes.oneOf(['purple', 'blue', 'green', 'orange', 'red']),
+	logo: PropTypes.string,
+	image: PropTypes.string,
+	logoText: PropTypes.string,
+	routes: PropTypes.arrayOf(PropTypes.object),
+	open: PropTypes.bool
+}
---
+++ src/views/Discover/ChannelResearchTemp.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import CardBody from '../../components/Card/CardBody.js'
+import { makeStyles } from '@material-ui/core/styles'
+import Hidden from '@material-ui/core/Hidden'
+import styles from '../../assets/jss/material-dashboard-react/views/iconsStyle.js'
+
+const useStyles = makeStyles(styles)
+
+function ChannelResearchTemp(props) {
+	const classes = useStyles()
+
+	return (
+		<CardBody>
+			<Hidden only={['sm', 'xs']}>
+				<iframe
+					className={classes.iframe}
+					src='https://datastudio.google.com/embed/reporting/1c7y-ayVXrgmrkfPEe1bw2kJDmRKxnc5b/page/Fs5KB'
+					title='Brand Mentality'
+				>
+					<p>Your browser does not support iframes.</p>
+				</iframe>
+			</Hidden>
+		</CardBody>
+	)
+}
+
+export default ChannelResearchTemp
