070d06e43704a18b797ae845361e45c7ac1f5858
Jared Dahlke <jared@sightly.com>
2020-10-26 13:20:20 -0700

    remove lazy react loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import React, { Suspense, lazy } from 'react'
+import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import PrivateRoute from './pages/PrivateRoute.js'
 import { Provider } from 'react-redux'
 import configureStore from './redux/store/index.js'
 import { neutralColor } from './assets/jss/colorContants'
-//import Admin from '../src/layouts/Admin.js'
+import Admin from '../src/layouts/Admin.js'
 
-const Admin = lazy(() => import('../src/layouts/Admin'))
-const ResetPassword = lazy(() => import('./pages/ResetPassword'))
-const ChangePassword = lazy(() => import('./pages/ChangePassword'))
-const Login = lazy(() => import('./pages/Login'))
+import ResetPassword from './pages/ResetPassword'
+import ChangePassword from './pages/ChangePassword'
+import Login from './pages/Login'
 
 const store = configureStore()
 
@@ -34,29 +33,22 @@ function App() {
 		<Provider store={store}>
 			<Router>
 				<div>
-					<Suspense fallback={<LoaderPage />}>
-						<Route exact path='/' component={Login} />
-						<Route path='/login' component={Login} />
-					</Suspense>
+					<Route exact path='/' component={Login} />
+					<Route path='/login' component={Login} />
 
-					<Suspense fallback={<LoaderPage />}>
-						<Route path='/resetPassword' component={ResetPassword} />
-					</Suspense>
+					<Route path='/resetPassword' component={ResetPassword} />
 
-					<Suspense fallback={<LoaderPage />}>
-						<Route
-							path='/changePassword/:userId/:token'
-							render={({ match }) => (
-								<ChangePassword
-									userId={match.params.userId}
-									token={match.params.token}
-								/>
-							)}
-						/>
-					</Suspense>
-					<Suspense fallback={<LoaderPage />}>
-						<PrivateRoute path='/admin' component={Admin} />
-					</Suspense>
+					<Route
+						path='/changePassword/:userId/:token'
+						render={({ match }) => (
+							<ChangePassword
+								userId={match.params.userId}
+								token={match.params.token}
+							/>
+						)}
+					/>
+
+					<PrivateRoute path='/admin' component={Admin} />
 				</div>
 			</Router>
 		</Provider>
