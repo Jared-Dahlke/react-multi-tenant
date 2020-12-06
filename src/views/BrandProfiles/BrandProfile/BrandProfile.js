89b19fb4a3163047549651044ed32cc8be435762
Jared Dahlke <jared@sightly.com>
2020-12-06 12:42:19 -0800

    use visibility hook

--- src/views/BrandProfiles/BrandProfile/BrandProfile.js
+++ src/views/BrandProfiles/BrandProfile/BrandProfile.js
@@ -21,6 +21,7 @@ import { connect } from 'react-redux'
 import { GridList } from '@material-ui/core'
 import { useScroll } from 'react-scroll-hooks'
 import Steps from 'rsuite/lib/Steps'
+import useOnScreen from './useOnScreen'
 
 const mapDispatchToProps = (dispatch) => {
 	return {
@@ -50,22 +51,70 @@ const mapStateToProps = (state) => {
 	}
 }
 
+const brandProfileSteps = {
+	brandInformation: 'brandInformation',
+	competitors: 'competitors',
+	categories: 'categories',
+	topics: 'topics',
+	scenarios: 'scenarios',
+	opinions: 'opinions'
+}
+
 function BrandProfile(props) {
 	const containerRef = React.useRef()
+	const brandInformationRef = React.useRef()
 	const categoriesRef = React.useRef()
 	const topicsRef = React.useRef()
 	const scenariosRef = React.useRef()
 	const opinionsRef = React.useRef()
 	const competitorsRef = React.useRef()
-	const brandInformationRef = React.useRef()
-	const scrollSpeed = 40
+	const brandInformationVisible = useOnScreen(brandInformationRef)
+	const competitorsVisible = useOnScreen(competitorsRef)
+	const categoriesVisible = useOnScreen(categoriesRef)
+	const topicsVisible = useOnScreen(topicsRef)
+	const scenariosVisible = useOnScreen(scenariosRef)
+	const opinionsVisible = useOnScreen(opinionsRef)
+
+	React.useEffect(() => {
+		if (brandInformationVisible) {
+			setActiveStep(brandProfileSteps.brandInformation)
+			return
+		}
+		if (competitorsVisible) {
+			setActiveStep(brandProfileSteps.competitors)
+			return
+		}
+		if (categoriesVisible) {
+			setActiveStep(brandProfileSteps.categories)
+			return
+		}
+		if (topicsVisible) {
+			setActiveStep(brandProfileSteps.topics)
+			return
+		}
+		if (scenariosVisible) {
+			setActiveStep(brandProfileSteps.scenarios)
+			return
+		}
+		if (opinionsVisible) {
+			setActiveStep(brandProfileSteps.opinions)
+			return
+		}
+	}, [
+		brandInformationVisible,
+		competitorsVisible,
+		categoriesVisible,
+		topicsVisible,
+		opinionsVisible,
+		scenariosVisible
+	])
+
+	const scrollSpeed = 80
 	const { scrollToElement, scrollToY } = useScroll({
 		scrollSpeed,
 		containerRef,
 		verticalOffset: 0
 	})
-
-	const [isCreated, setIsCreated] = React.useState(false)
 
 	const [competitorsValid, setCompetitorsValid] = React.useState(false)
 
@@ -75,7 +124,14 @@ function BrandProfile(props) {
 
 	const [scenariosValid, setScenariosValid] = React.useState(false)
 
-	const handleStepsClick = (ref) => {
+	const [opinionsValid, setOpinionsValid] = React.useState(false)
+
+	const [activeStep, setActiveStep] = React.useState(
+		brandProfileSteps.brandInformation
+	)
+
+	const handleStepsClick = (step, ref) => {
+		//	setActiveStep(step)
 		scrollToElement(ref)
 	}
 
@@ -94,14 +150,33 @@ function BrandProfile(props) {
 		brandProfileId: ''
 	})
 
-	const springProps = useSpring({
-		from: {
-			opacity: 1
-		},
-		enter: { opacity: 0 },
-		leave: {
-			opacity: 0
-		}
+	const brandInfoProps = useSpring({
+		opacity: brandProfile.brandName.length > 0 ? 1 : 0
+	})
+
+	const competitorsProps = useSpring({
+		opacity: brandProfile.competitors ? 1 : 0
+		//	marginTop: brandProfile.competitors ? 20 : 500
+	})
+
+	const categoriesProps = useSpring({
+		opacity: brandProfile.categories ? 1 : 0
+		//	marginTop: brandProfile.categories ? 20 : 500
+	})
+
+	const topicsProps = useSpring({
+		opacity: brandProfile.topics ? 1 : 0
+		//	marginTop: brandProfile.categories ? 20 : 500
+	})
+
+	const opinionsProps = useSpring({
+		opacity: brandProfile.opinions ? 1 : 0
+		//	marginTop: brandProfile.categories ? 20 : 500
+	})
+
+	const scenariosProps = useSpring({
+		opacity: brandProfile.scenarios ? 1 : 0
+		//	marginTop: brandProfile.categories ? 20 : 500
 	})
 
 	React.useEffect(() => {
@@ -121,33 +196,70 @@ function BrandProfile(props) {
 					<Steps vertical>
 						<Steps.Item
 							title='Brand Information'
-							onClick={() => handleStepsClick(brandInformationRef)}
+							onClick={() =>
+								handleStepsClick(
+									brandProfileSteps.brandInformation,
+									brandInformationRef
+								)
+							}
 							style={{ cursor: 'pointer' }}
-							status={isCreated ? 'finish' : 'wait'}
+							status={
+								activeStep === brandProfileSteps.brandInformation
+									? 'process'
+									: 'wait'
+							}
 						/>
 						<Steps.Item
 							title='Competitors'
-							onClick={() => handleStepsClick(competitorsRef)}
+							onClick={() =>
+								handleStepsClick(brandProfileSteps.competitors, competitorsRef)
+							}
 							style={{ cursor: 'pointer' }}
-							status={competitorsValid ? 'finish' : 'wait'}
+							status={
+								activeStep === brandProfileSteps.competitors
+									? 'process'
+									: 'wait'
+							}
 						/>
 						<Steps.Item
 							title='Categories'
-							onClick={() => handleStepsClick(categoriesRef)}
+							onClick={() =>
+								handleStepsClick(brandProfileSteps.categories, categoriesRef)
+							}
 							style={{ cursor: 'pointer' }}
-							status={categoriesValid ? 'finish' : 'wait'}
+							status={
+								activeStep === brandProfileSteps.categories ? 'process' : 'wait'
+							}
 						/>
 						<Steps.Item
 							title='Topics'
-							onClick={() => handleStepsClick(topicsRef)}
+							onClick={() =>
+								handleStepsClick(brandProfileSteps.topics, topicsRef)
+							}
 							style={{ cursor: 'pointer' }}
-							status={topicsValid ? 'finish' : 'wait'}
+							status={
+								activeStep === brandProfileSteps.topics ? 'process' : 'wait'
+							}
 						/>
 						<Steps.Item
 							title='Scenarios'
-							onClick={() => handleStepsClick(scenariosRef)}
+							onClick={() =>
+								handleStepsClick(brandProfileSteps.scenarios, scenariosRef)
+							}
 							style={{ cursor: 'pointer' }}
-							status={scenariosValid ? 'finish' : 'wait'}
+							status={
+								activeStep === brandProfileSteps.scenarios ? 'process' : 'wait'
+							}
+						/>
+						<Steps.Item
+							title='Opinions'
+							onClick={() =>
+								handleStepsClick(brandProfileSteps.opinions, opinionsRef)
+							}
+							style={{ cursor: 'pointer' }}
+							status={
+								activeStep === brandProfileSteps.opinions ? 'process' : 'wait'
+							}
 						/>
 					</Steps>
 				</Grid>
@@ -163,45 +275,57 @@ function BrandProfile(props) {
 						<GridItem xs={12} sm={12} md={10}>
 							<div style={{ padding: 10 }}>
 								<div ref={brandInformationRef} />
-								<animated.div style={springProps}>
+								<animated.div style={brandInfoProps}>
 									<BasicInfo
 										brandProfile={brandProfile}
 										industryVerticals={props.industryVerticals}
 									/>
 								</animated.div>
 
-								<div ref={competitorsRef} />
+								<div ref={competitorsRef} style={{ marginTop: 60 }}>
+									<animated.div style={competitorsProps}>
+										<TopCompetitors
+											setCompetitorsValid={setCompetitorsValid}
+											brandProfile={brandProfile}
+										/>
+									</animated.div>
+								</div>
 
-								<TopCompetitors
-									setCompetitorsValid={setCompetitorsValid}
-									brandProfile={brandProfile}
-								/>
+								<div ref={categoriesRef} style={{ marginTop: 60 }}>
+									<animated.div style={categoriesProps}>
+										<Categories
+											categoriesValid={categoriesValid}
+											setCategoriesValid={setCategoriesValid}
+											brandProfile={brandProfile}
+										/>
+									</animated.div>
+								</div>
 
-								<div ref={categoriesRef} style={{ marginTop: 30 }} />
+								<div ref={topicsRef} style={{ marginTop: 60 }}>
+									<animated.div style={topicsProps}>
+										<Topics
+											setTopicsValid={setTopicsValid}
+											brandProfile={brandProfile}
+										/>
+									</animated.div>
+								</div>
 
-								<Categories
-									categoriesValid={categoriesValid}
-									setCategoriesValid={setCategoriesValid}
-									brandProfile={brandProfile}
-								/>
-
-								<div ref={topicsRef} style={{ marginTop: 30 }} />
-
-								<Topics
-									setTopicsValid={setTopicsValid}
-									brandProfile={brandProfile}
-								/>
-
-								<div ref={scenariosRef} style={{ marginTop: 30 }} />
-								<Scenarios
-									setScenariosValid={setScenariosValid}
-									brandProfile={brandProfile}
-								/>
-								<div ref={opinionsRef} style={{ marginTop: 30 }} />
-								<Opinions
-									//setScenariosValid={setScenariosValid}
-									brandProfile={brandProfile}
-								/>
+								<div ref={scenariosRef} style={{ marginTop: 60 }}>
+									<animated.div style={scenariosProps}>
+										<Scenarios
+											setScenariosValid={setScenariosValid}
+											brandProfile={brandProfile}
+										/>
+									</animated.div>
+								</div>
+								<div ref={opinionsRef} style={{ marginTop: 60 }}>
+									<animated.div style={opinionsProps}>
+										<Opinions
+											//setScenariosValid={setScenariosValid}
+											brandProfile={brandProfile}
+										/>
+									</animated.div>
+								</div>
 							</div>
 						</GridItem>
 					</GridList>
