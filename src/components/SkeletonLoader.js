0bbe0b3748f834425efa2dd23b74d1ad6fe7a2c8
Jared Dahlke <jared@sightly.com>
2021-01-21 08:50:22 -0800

    improve more imports to save bundleSize

--- src/components/SkeletonLoader.js
+++ src/components/SkeletonLoader.js
@@ -1,5 +1,4 @@
 import { List } from 'react-content-loader'
-import { grayColor } from '../assets/jss/material-dashboard-react'
 import React from 'react'
 import { neutralLightColor } from '../assets/jss/colorContants'
 
