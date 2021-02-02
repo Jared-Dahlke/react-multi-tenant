200c191725d81e01248e9104be19a45155eb820e
Jared Dahlke <jared@sightly.com>
2021-02-02 11:36:53 -0800

    remove unused prop

--- src/components/CustomPanel.js
+++ src/components/CustomPanel.js
@@ -3,7 +3,7 @@ import Panel from 'rsuite/lib/Panel'
 import { neutralLightColor } from '../assets/jss/colorContants'
 
 const CustomPanel = (props) => {
-	const { className, children, plain, profile, chart, flex1, ...rest } = props
+	const { className, children, plain, profile, chart, ...rest } = props
 	return (
 		<Panel {...rest} style={{ backgroundColor: neutralLightColor }}>
 			{children}
